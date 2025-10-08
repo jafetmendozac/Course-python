8dc815dba1644b7834d2a0471a847bb56f8aedc6
jafetmendozac <abrahamjoelmc@gmail.com>
2025-10-08 16:31:58 -0500

    feat: first exercise

---
+++ ps1/exercises/deep.py
@@ -0,0 +1,7 @@
+answer = input("What is the Answer to the Great Question of Life, the Universe and Everything? ")
+
+
+if(answer == "42" or answer.lower() == "twenty two" or answer == "twenty-two"):
+  print("Yes")
+else:
+  print("No")
