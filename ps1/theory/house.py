fcfd0196e157256c69cad8233b47e51000f60480
jafetmendozac <abrahamjoelmc@gmail.com>
2025-10-08 13:38:28 -0500

    feat: theory of second class

---
+++ ps1/theory/house.py
@@ -0,0 +1,25 @@
+# name = input("What's your name? ")
+
+# if name == "Harry" or name == "Hermione" or name == "Ron":
+#   print("Gryffindor")
+# # elif name == "Hermione":
+# #   print("Gryffindor")
+# # elif name == "Ron":
+# #   print("Gryffindor")
+# elif name == "Draco":
+#   print("Slytherin")
+# else: 
+#   print("Who?")
+
+
+
+name = input("What's your name? ")
+
+match name:
+  case "Harry" | "Hermanione" | "Ron":
+    print("Gryffindor")
+  case "Draco":
+    print("Slytherin")
+  case _:
+    print("Who?")
+
---
+++ ps1/theory/parity.py
@@ -0,0 +1,23 @@
+# x = input("What's x? ")
+
+
+# if x % 2 == 0:
+#   print("Even")
+# else:
+#   print("Odd")
+
+
+
+
+def main():
+  x = int(input("What's x? "))
+  if is_even(x):
+    print("Even")
+  else:
+    print("Odd")
+
+def is_even(n):
+  # return (n%2) == 0 #True False
+  return True if n % 2 == 0 else False
+
+main()
